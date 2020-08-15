406752f8bc629483d0004e6bab2fd9e11b6d436f
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2020-08-15 20:31:18 -0300

    fix(gateway): Fix error handling, returning status text

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponserTrait;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -18,6 +19,76 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     use ApiResponserTrait;
+
+    /**
+     * Returns a list of status codes and messages.
+     *
+     * @var array
+     */
+    const STATUS_TEXTS = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',            // RFC2518
+        103 => 'Early Hints',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',          // RFC4918
+        208 => 'Already Reported',      // RFC5842
+        226 => 'IM Used',               // RFC3229
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',    // RFC7238
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',                                               // RFC2324
+        421 => 'Misdirected Request',                                         // RFC7540
+        422 => 'Unprocessable Entity',                                        // RFC4918
+        423 => 'Locked',                                                      // RFC4918
+        424 => 'Failed Dependency',                                           // RFC4918
+        425 => 'Too Early',                                                   // RFC-ietf-httpbis-replay-04
+        426 => 'Upgrade Required',                                            // RFC2817
+        428 => 'Precondition Required',                                       // RFC6585
+        429 => 'Too Many Requests',                                           // RFC6585
+        431 => 'Request Header Fields Too Large',                             // RFC6585
+        451 => 'Unavailable For Legal Reasons',                               // RFC7725
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',                                     // RFC2295
+        507 => 'Insufficient Storage',                                        // RFC4918
+        508 => 'Loop Detected',                                               // RFC5842
+        510 => 'Not Extended',                                                // RFC2774
+        511 => 'Network Authentication Required',                             // RFC6585
+    ];
 
     /**
      * A list of the exception types that should not be reported.
@@ -43,7 +114,7 @@ class Handler extends ExceptionHandler
                 $code = $exception->getStatusCode();
 
                 return [
-                    Response::$statusTexts($code),
+                    self::STATUS_TEXTS[$code],
                     $code,
                 ];
             },
@@ -72,6 +143,12 @@ class Handler extends ExceptionHandler
                     $exception->validator->errors()->getMessages(),
                     Response::HTTP_UNPROCESSABLE_ENTITY,
                 ];
+            },
+            ClientException::class => function (ClientException $exception) {
+                return $this->errorMessage(
+                    $exception->getResponse()->getBody(),
+                    $exception->getCode()
+                );
             },
         ];
     }
@@ -104,10 +181,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($handler = $this->exceptionHandlers()[\get_class($exception)] ?? null) {
-            $params = $handler($exception);
+        foreach ($this->exceptionHandlers() as $class => $handler) {
+            if (is_subclass_of($exception, $class)) {
+                $params = $handler($exception);
 
-            return \call_user_func_array([$this, 'errorResponse'], $params);
+                return $params instanceof Response
+                    ? $params
+                    : \call_user_func_array([$this, 'errorResponse'], $params);
+            }
         }
 
         if (env('APP_DEBUG', false)) {
